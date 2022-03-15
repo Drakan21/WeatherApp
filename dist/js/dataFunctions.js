f26a2f603a94790140a9a707af2d51cba5891150
Rudi <rudi@dev.com>
2022-03-15 06:54:42 -0700

    --

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -1,76 +1,72 @@
 export const setLocationObject = (location, coordinates) => {
-    const { lat, lon, name, unit } = coordinates;
-    location.setLatitude(lat);
-    location.setLongitude(lon);
-    location.setName(name);
-    if(unit) location.setUnits(unit);
-}
+  const { lat, lon, name, unit } = coordinates;
+  location.setLatitude(lat);
+  location.setLongitude(lon);
+  location.setName(name);
+  if (unit) location.setUnits(unit);
+};
 
 export const getHomeLocation = (key) => {
-    return JSON.parse(localStorage.getItem(key));
-}
+  return JSON.parse(localStorage.getItem(key));
+};
 export const setHomeLocation = (key, location) => {
-    localStorage.setItem(key, JSON.stringify(location));
-}
+  localStorage.setItem(key, JSON.stringify(location));
+};
 
 export const cleanText = (value) => {
-    const t = document.createElement('span');
-    t.innerText = value;
-    value = t.innerHTML;
-    const entryText = value.replaceAll(/ {2,}/g, ' ').trim();
-    return entryText;
-}
+  const t = document.createElement("span");
+  t.innerText = value;
+  value = t.innerHTML;
+  const entryText = value.replaceAll(/ {2,}/g, " ").trim();
+  return entryText;
+};
 export const toProperCase = (text) => {
-    if(!text) return "";
-    const words = text.split(" ");
-    const properWords = words.map(word => {
-        return word.charAt(0).toUpperCase() + word.slice(1);
+  if (!text) return "";
+  const words = text.split(" ");
+  const properWords = words.map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+  return properWords.join(" ");
+};
+
+export const parseValue = (value, sym = "") => {
+  return `${Math.round(Number(value))}${sym}`;
+};
+
+export const getWeatherFromCoords = async (location) => {
+  const urlDataObj = {
+    lat: location.getLatitude(),
+    lon: location.getLongitude(),
+    units: location.getUnits(),
+  };
+  try {
+    const w_data = await fetch("./.netlify/functions/get_weather", {
+      method: "POST",
+      body: JSON.stringify(urlDataObj),
     });
-    return properWords.join(" ");
-}
-
-export const parseValue = (value, sym="") => {
-    return `${Math.round(Number(value))}${sym}`;
-}
-
-export const getWeatherFromCoords = async (location)=> {
-    const urlDataObj = {
-        lat: location.getLatitude(),
-        lon: location.getLongitude(),
-        units: location.getUnits()
-    };
-    console.log(urlDataObj);
-    try {
-        console.log(JSON.stringify(urlDataObj))
-        const w_data = await fetch("./.netlify/functions/get_weather", {
-            method: 'POST',
-            body: JSON.stringify(urlDataObj)
-        });
-        console.log(w_data);
-        const j_data = await w_data.json();
-        return j_data;
-    } catch(err) {
-        console.error(err);
-    }
-}
-
+    console.log(w_data);
+    const j_data = await w_data.json();
+    return j_data;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 /* request functions */
 export const getCoordsFromApi = async (entry, units) => {
-    const urlDataObj = {
-        entry,
-        units    
-    };
-    console.log(urlDataObj);
-    try {
-        
-        const w_data = await fetch("./.netlify/functions/get_coords", {
-            method: 'POST',
-            body: JSON.stringify(urlDataObj)
-        });
-        const j_data = await w_data.json();
-        return j_data;
-    } catch(err) {
-        console.error(err);
-    }
-}
+  const urlDataObj = {
+    entry,
+    units,
+  };
+  console.log(urlDataObj);
+  try {
+    const w_data = await fetch("./.netlify/functions/get_coords", {
+      method: "POST",
+      body: JSON.stringify(urlDataObj),
+    });
+    const j_data = await w_data.json();
+    return j_data;
+  } catch (err) {
+    console.error(err);
+  }
+};
--- serverless/get_coords.js
+++ serverless/get_coords.js
@@ -1,27 +1,28 @@
+const fetch = import("node-fetch");
 
-const fetch = import('node-fetch');
-
-const { WEATHER_API_KEY }  = process.env;
+const { WEATHER_API_KEY } = process.env;
 
 exports.handler = async (event, context) => {
-    const params = JSON.parse(event.body);
-    const { entry, units } = params;
-    const regex = /^\d+$/g;
-    const flag = regex.test(entry) ? 'zip' : 'q';
-    const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entry}&units=${units}&appid=${WEATHER_API_KEY}`;
-    const encUrl = encodeURI(url);
+  const params = JSON.parse(event.body);
+  const { entry, units } = params;
+  const regex = /^d+$/g;
+  const flag = regex.test(entry) ? "zip" : "q";
+  const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entry}&units=${units}&appid=${WEATHER_API_KEY}`;
+  const encUrl = encodeURI(url);
 
-    try {
-        const w_data = await fetch(encUrl);
-        const j_data = await w_data.json();
-        return {
-            statusCode: 200,
-            body: JSON.stringify(j_data)
-        };
-    } catch(err) {
-        return {
-            statusCode: 422,
-            body: err.stack
-        }
-    }
-}
+  try {
+    const wdata = await fetch(encUrl);
+    const jdata = await wdata.json();
+    console.log(wdata);
+    console.log(jdata);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(jdata),
+    };
+  } catch (err) {
+    return {
+      statusCode: 422,
+      body: err.stack,
+    };
+  }
+};
