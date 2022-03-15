34c3e9a54f500f47422e0a9d8a7618df8d37d9b3
Rudi <rudi@dev.com>
2022-03-15 07:24:11 -0700

    ---

--- serverless/get_weather.js
+++ serverless/get_weather.js
@@ -11,7 +11,7 @@ exports.handler = async (event, context) => {
     // const jdata = await wdata.json();
     return {
       statusCode: 200,
-      body: { data: "This is weather data" },
+      body: "This is weather data",
     };
   } catch (err) {
     return {
