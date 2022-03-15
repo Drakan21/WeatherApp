17d5c0e8578764e39f5303c6c6815b1c65276337
Rudi <rudi@dev.com>
2022-03-15 07:06:05 -0700

    ---

--- serverless/get_coords.js
+++ serverless/get_coords.js
@@ -15,7 +15,7 @@ exports.handler = async (event, context) => {
     const jdata = await wdata.json();
     return {
       statusCode: 200,
-      body: JSON.stringify(jdata),
+      body: jdata ? JSON.stringify(jdata) : "",
     };
   } catch (err) {
     return {
